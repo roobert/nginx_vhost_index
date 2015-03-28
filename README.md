3a134b4add3bc7bf7d8ac9606ecfa97b90cde99a
Rob Wilson <roobert@gmail.com>
2015-03-28 12:07:06 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+http://index.localhost
---
+++ app.rb
@@ -0,0 +1,55 @@
+#!/usr/bin/env ruby
+
+require 'sinatra'
+require 'httparty'
+require 'redcarpet'
+require 'ostruct'
+
+require 'haml'
+require 'sass'
+
+require 'ap'
+require 'pp'
+require 'yaml'
+
+# this auto-reloads files with changed mtime
+Sinatra::Application.reset!
+
+helpers do
+  def http_status?(vhost)
+    true if HTTParty.get("http://#{vhost}").code.to_s =~ /^2../
+  end
+end
+
+before do
+  @vhosts = {}
+
+  Dir["/etc/nginx/sites-enabled/*"].each do |file|
+    next unless File.file?(file)
+
+    File.open(file, 'r') do |file_contents|
+      matches = file_contents.grep(/server_name.*;/)
+
+      next if matches.nil?
+
+      matches.inject(@vhosts) do |vhosts, match|
+        vhost = match.gsub(/ *server_name */, '').gsub(/;$/, '').chomp
+
+        next if vhost == 'index.localhost'
+
+        vhosts[vhost] = http_status?(vhost) ? :success : :failure
+        vhosts
+      end
+    end
+  end
+end
+
+set :haml, { :ugly=>true }
+
+get '/css/:style.css' do
+  scss :"#{params[:style]}"
+end
+
+get '/' do
+  haml :index
+end
